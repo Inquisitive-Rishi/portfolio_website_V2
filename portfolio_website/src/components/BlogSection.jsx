366913622e653f878648babecbf3a90f3f517467
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-26 23:11:43 +0530

    finish blog section initial structure

--- portfolio_website/src/components/BlogSection.jsx
+++ portfolio_website/src/components/BlogSection.jsx
@@ -9,18 +9,20 @@ console.log(featuredData);
 
 function BlogSection() {
   return (
-   <div className="min-h-[700px]"> 
+   <div className="p-10"> 
     <SectionTitle text="Featured"/>
+    <div className="flex justify-around  max-[900px]:flex-col">
     {featuredData.map(data => {
         return (
-            <>
+            <div key={data.title} className="w-[250px]">
             <Subtitle key={data.title} text={data.title}/>
-            <div className="w-[200px] h-[250px] bg-slate-400"></div>
+            <div className="w-[250px] h-[250px] bg-slate-400 my-2"></div>
             <ParagraphNormal key={data.text} text={data.text}/>
             <Link text="Read the full story"/>
-            </>
+            </div>
         )
     })}
+    </div>
    </div>
   )
 }
--- portfolio_website/src/components/Link.jsx
+++ portfolio_website/src/components/Link.jsx
@@ -1,6 +1,6 @@
 function Link({styles,text}) {
   return (
-    <a href="#" className="font-bold text-red-500 hover:underline max-[790px]:ml-[270px]">{text}</a>
+    <a href="#" className="font-bold text-red-500 hover:underline text-nowrap ml-0 max-[790px]:ml-[270px]">{text}</a>
 )
 }
 
