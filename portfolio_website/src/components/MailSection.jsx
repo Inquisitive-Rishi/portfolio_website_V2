54ff20c56d8d56fdecf6ca175e23f8d1052d408a
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-30 22:55:36 +0530

    add form element

--- portfolio_website/src/components/MailSection.jsx
+++ portfolio_website/src/components/MailSection.jsx
@@ -1,26 +1,23 @@
 import Button from "./Button"
 import emailImg  from "../assets/social/email.svg"
+import ProjectTitle from "./paragraphs/ProjectTitle"
 
 function MailSection() {
     const styles = {
         outline: "none",
-        border: "none",
-        borderRadius: "10px",
+        border: "2px solid black",
         width: "100%",
-        padding: ".6em",
-        backgroundColor: "lightgrey",
+        padding: "0.4rem",
         color: "black"
     }
   return (
-    <div className="flex justify-around mt-20">
-        <div>
-            <img src={emailImg} alt="email image" className="w-full h-auto"/>
-        </div>
+    <div className="flex justify-between mt-20">
+        <ProjectTitle title="🤓 We'd love to hear from you"/>
         <form action="#" className="flex flex-col gap-5 max-w-[100%] items-center">
             <input type="text" name="fullname" placeholder="name" style={styles}/>
             <input type="text" name="email" placeholder="email" style={styles}/>
             <textarea name="text" id="" cols="30" rows="10" style={styles} placeholder="Hello Rishi...."></textarea>
-            <Button text="Send"/>
+            <button className="p-2 bg-red-500 text-white w-[100%] hover:font-bold">Submit</button>
         </form>
     </div>
   )
