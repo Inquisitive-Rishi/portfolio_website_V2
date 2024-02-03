cdea924ca5a469008b85f183ef996a5f40fcdefc
Rishi Raj <rishiraj007bgp@gmail.com>
2024-02-03 22:18:52 +0530

    show hamburger on 790px and small

--- portfolio_website/src/components/Navbar.jsx
+++ portfolio_website/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import NavButton from "./NavButton";
 import * as data from '../data.js'
+import hamImg from "../assets/hamburger.png"
 
 function Navbar() {
 
     return (
-        <div className="flex gap-8 justify-center p-2 max-[790px]:flex-col items-center px-20 font-bold">
+        <div className="flex gap-8 justify-center p-2 items-center px-20 font-bold">
             <p className="mr-auto text-3xl">Rishi Raj</p>
-            <div>
+            <div><img src={hamImg} className="md-[790px]:hidden" /></div>
+            <div className="max-[790px]:hidden">
             {data.links.map(link => <NavButton txt={link} key={link}/>)}
             </div>
         </div>
